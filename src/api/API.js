773ceb55d41e6cc9af77c969eda85b72f18919f3
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2020-04-08 23:09:06 +0200

    task preparation

---
+++ src/api/API.js
@@ -0,0 +1,15 @@
+/**
+ * Response example:
+   [
+        {
+            "albumId": 1,
+            "id": 1,
+            "title": "accusamus beatae ad facilis cum similique qui sunt",
+            "url": "https://via.placeholder.com/600/92c952",
+            "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+        },
+        ...
+    ]
+ */
+
+export const API = 'https://jsonplaceholder.typicode.com/photos'
---
+++ src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Card.scss';
+
+const Card = () => {
+    return (
+        <article className="Card"></article>
+    )
+}
+
+export default Card;
